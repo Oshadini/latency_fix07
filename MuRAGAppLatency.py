54e7949b2ffc129a6486c380e4a73ae6df3dfac5
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-08 02:29:39 +0530

    Update MuRAGAppLatency.py

--- MuRAGAppLatency.py
+++ MuRAGAppLatency.py
@@ -235,45 +235,45 @@ if uploaded_file is not None:
       return msg.content
     @st.cache_data()
     def generate_img_summaries(path):
-    """
-    Generate summaries and base64 encoded strings for images
-    path: Path to list of .jpg files extracted by Unstructured
-    """
+        """
+        Generate summaries and base64 encoded strings for images
+        path: Path to list of .jpg files extracted by Unstructured
+        """
+        
+        # Store base64 encoded images
+        img_base64_list = []
+        
+        # Store image summaries
+        image_summaries = []
+        
+        # Prompt
+        prompt = """You are an assistant tasked with summarizing images for retrieval. \
+        These summaries will be embedded and used to retrieve the raw image. \
+        Give a concise summary of the image that is well optimized for retrieval."""
+        
+        # Apply to images
+        for img_file in os.listdir(path):
+            if img_file.endswith(".jpg"):
+                img_path = os.path.join(path, img_file)
+                base64_image = encode_image(img_path)
+                img_base64_list.append(base64_image)
+                image_summaries.append(image_summarize(base64_image, prompt))
+        
+        return img_base64_list, image_summaries
+        
+        if 'image_elements' not in st.session_state:
+            with st.spinner("Generating Images summaries......"):
+            img_base64_list, image_summaries = generate_img_summaries(fpath)
     
-    # Store base64 encoded images
-    img_base64_list = []
-    
-    # Store image summaries
-    image_summaries = []
-    
-    # Prompt
-    prompt = """You are an assistant tasked with summarizing images for retrieval. \
-    These summaries will be embedded and used to retrieve the raw image. \
-    Give a concise summary of the image that is well optimized for retrieval."""
-    
-    # Apply to images
-    for img_file in os.listdir(path):
-        if img_file.endswith(".jpg"):
-            img_path = os.path.join(path, img_file)
-            base64_image = encode_image(img_path)
-            img_base64_list.append(base64_image)
-            image_summaries.append(image_summarize(base64_image, prompt))
-    
-    return img_base64_list, image_summaries
-    
-    if 'image_elements' not in st.session_state:
-        with st.spinner("Generating Images summaries......"):
-        img_base64_list, image_summaries = generate_img_summaries(fpath)
-
-        st.session_state["img_base64_list"] = img_base64_list
-        st.session_state["image_summaries"] = image_summaries
-    else:
-        img_base64_lists = st.session_state["img_base64_list"]  
-        image_summaries = st.session_state["image_summaries"]  
-    
-    st.write(f"{bullet_point} \t\tImage summaries generation completed") 
-    st.write(f"{bullet_point} Summary generation process completed")  
-    
+            st.session_state["img_base64_list"] = img_base64_list
+            st.session_state["image_summaries"] = image_summaries
+        else:
+            img_base64_lists = st.session_state["img_base64_list"]  
+            image_summaries = st.session_state["image_summaries"]  
+        
+        st.write(f"{bullet_point} \t\tImage summaries generation completed") 
+        st.write(f"{bullet_point} Summary generation process completed")  
+        
     
     @st.cache_resource()
     def create_multi_vector_retriever(
